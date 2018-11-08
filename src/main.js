510b1923e3b5e86028f3bc7a9d62a0cada2c1379
DESKTOP-4UDOJS8\Orell <orell.endres@stud.hslu.ch>
2018-11-08 14:53:53 +0100

    first commit

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
+import {createClient} from 'contentful';
 
 Vue.config.productionTip = false;
 
@@ -8,3 +9,12 @@ new Vue({
   router,
   render: h => h(App)
 }).$mount("#app");
+
+window.contentfulClient = createClient({
+    accessToken: '64d6a750c7ae5a7c93603911e56166b198ce5ab94be05261848e8a280ba8972c',
+    space: '7la5sjify8om'
+});
+
+contentfulClient.getEntries()
+.then((response) => console.log(response.items))
+.catch(console.error);
