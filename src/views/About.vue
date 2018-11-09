61f6a5ef134688a867d7b02d97497cc3f26ef282
DESKTOP-4UDOJS8\Orell <orell.endres@stud.hslu.ch>
2018-11-09 19:06:05 +0100

    test about

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,3 +1,29 @@
 <template>
-  <div class="about"><h1>This is an about page</h1></div>
+  <div class="about"><h1>This is an about page</h1>
+
+
+    <div class="test"><h2>test test test</h2></div>
+
+
+  </div>
+
+
 </template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from "@/components/HelloWorld.vue";
+
+export default {
+  name: "About",
+  components: {
+    HelloWorld
+  },
+  mounted: function(){
+
+
+
+
+  }
+};
+</script>
