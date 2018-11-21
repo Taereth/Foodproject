ccd6c0b5ddaf7fe5cd12dd44c44ba646967167d3
DESKTOP-4UDOJS8\Orell <orell.endres@stud.hslu.ch>
2018-11-21 14:39:24 +0100

    P5JS kinda workin

--- src/components/Draw.vue
+++ src/components/Draw.vue
@@ -1,27 +1,43 @@
 <template>
-  <div class="about"><h1>This is an about page</h1>
-
-
-    <div class="test"><h2>test test test</h2></div>
-    <div id="p5Container" class="canvas-container"></div>
-
-
+  <div class="Draw">
+      <div ref="p5"></div>
   </div>
-
-
 </template>
-
 <script>
 
+var p = false;
 
 export default {
   name: "Draw",
-  mounted: function(){
-
-
-
-    console.log("brah");
-
+  data: function() {
+    return {};
+  },
+  methods: {},
+  mounted: function() {
+    console.log(this.$refs.p5);
+    console.log(this.$route);
+    let s = function(sketch) {
+      sketch.setup = function() {
+        sketch.createCanvas(200, 200);
+        sketch.strokeWeight(10)
+	      sketch.stroke(0);
+      };
+      sketch.draw = function() {
+        console.log(p);
+      };
+      sketch.mousePressed = function() {
+        p = true;
+      };
+      sketch.mouseReleased = function() {
+        p = false;
+      };
+      sketch.mouseDragged = function() {
+        console.log("yo");
+        sketch.strokeWeight(5);
+	      sketch.line(sketch.mouseX, sketch.mouseY, sketch.pmouseX, sketch.pmouseY);
+      }
+    };
+    let myp5 = new p5(s, this.$refs.p5);
   }
 };
 </script>
