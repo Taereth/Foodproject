3d715147a73d1ea812682cf376c56a012b35a844
DESKTOP-4UDOJS8\Orell <orell.endres@stud.hslu.ch>
2018-11-22 15:39:30 +0100

    Redirect to ListMode

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -50,7 +50,7 @@ export default {
       entries.items.forEach((entry)=>{
       if(entry.fields.passwort == this.passwordinput){
         document.cookie = "username="+this.usernameinput;
-        this.$router.push("/homescreen");
+        this.$router.push("/listmode");
       }
       else{
         this.seen = true;
